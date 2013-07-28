75fa2c74f2ba1d507312075f6bc536ac49323f94
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2013-07-28 23:33:28 +0200

    - Testing with have_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,7 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contacts') }
+    it { should have_selector('h1', text: 'Contacts') }
     it { should have_title(full_title('Contact')) }
   end
 end
