3109e16e1a21fc573fede45dfb44cbc77e5befc9
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2013-07-29 18:05:20 +0200

    - Stubbed profile page with sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def to_s
+    "#{self.id}. #{self.name}, #{self.email}"
+  end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do
+  factory :user do
+    name      "Pluto il Cane"
+    email     "pluto@disney.com"
+    password  "topolino"
+    password_confirmation "topolino"
+  end
+end
